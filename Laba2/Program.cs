29be306af85a99cea431f426ebfac678b60287a5
kitty666666 <skatusha04@gmail.com>
2023-10-08 14:04:11 +0300

    Добавлен счетчик букв в строке

--- Laba2/Program.cs
+++ Laba2/Program.cs
@@ -7,6 +7,11 @@ class Program
         Console.Write("Введите строку: ");
         string inputString = Console.ReadLine();
 
+        int length = inputString.Length;
+
         Console.WriteLine("Вы ввели: " + inputString);
+        Console.WriteLine("Количество символов в строке: " + length);
+
+        Console.ReadKey(); // Чтобы программа не закрылась сразу после вывода результата
     }
 }
